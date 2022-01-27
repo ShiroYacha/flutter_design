c50745652301dfbbcba18a3b20ef7764bc0c6644
YAPMZHA <min.zhao@epfl.ch>
2022-01-27 20:08:15 +0100

    Clean up project

--- docs/1_overview.md
+++
@@ -1,6 +0,0 @@
-# Flutter design system
-
-```yaml
-
-```
-
--- flutter_design_codegen/lib/src/extension.dart
+++
@@ -1,5 +0,0 @@
-import 'package:recase/recase.dart';
-
-extension StringExtension on String {
-  String get pascalCase => ReCase(this).pascalCase;
-}
