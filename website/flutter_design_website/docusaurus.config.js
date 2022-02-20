cc52f1e1091a30592e9d7af61b2db6aa6d8abfe1
ShiroYacha <sebzhaomin@gmail.com>
2022-02-20 18:08:15 +0100

    Include google analytics to landing page, write coming soon and reconfigure dependencies

--- website/flutter_design_website/docusaurus.config.js
+++ website/flutter_design_website/docusaurus.config.js
@@ -26,14 +26,18 @@ const config = {
           // Please change this to your repo.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-S8W5YK0FNP',
+          anonymizeIP: true,
         },
       }),
     ],
@@ -55,7 +59,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          // { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/ShiroYacha/flutter_design/',
             label: 'GitHub',
@@ -91,10 +95,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/ShiroYacha/flutter_design/',
