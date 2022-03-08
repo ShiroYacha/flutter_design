d963b091676fce4567293f70c092f1d52db8bba9
ShiroYacha <sebzhaomin@gmail.com>
2022-03-08 23:34:23 +0100

    Improve website banner responsiveness, add caution text on get started

--- website/src/components/Banner/index.tsx
+++ website/src/components/Banner/index.tsx
@@ -6,52 +6,90 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import { CodeSnippet } from "../CodeSnippet";
 import { VideoSnippet } from "../VideoSnippet";
 import DesignSampleCode from '!!raw-loader!/static/snippets/buttons.dart';
+import { withBreakpoints } from 'react-breakpoints'
 
-
-export const Banner: React.FC = () => {
+const _Banner = ({ breakpoints, currentBreakpoint }) => {
   const { siteConfig } = useDocusaurusContext();
-
-  return (
+  const breakpoint = breakpoints[currentBreakpoint];
+  return breakpoint < breakpoints.desktopWide && breakpoint > breakpoints.mobileLandscape ? (<div>
+    <div className="banner__content_centered">
+      {logo(siteConfig)}
+      {headline()}
+      <div>
+        {getStarted()}
+        {playWithDemo()}
+      </div>
+    </div>
     <header className="banner">
-      <div className="banner__content">
-        <h1 className="banner__logo">
-          <img src="img/logo.png" alt="flutter_design" />
-          {siteConfig.title}
-        </h1>
-
-        <h1 className="banner__headline">
-          <Translate id="home.tagline">
-            Supercharge Flutter UX development
-          </Translate>
-        </h1>
-
-        <div>
-          <Link className="banner__cta" to={useBaseUrl("docs/get_started")}>
-            <Translate id="home.get_started">Get Started</Translate>
-          </Link>
-          <Link className="banner__ctb" to="https://flutter-design-7b479.web.app/">
-            <Translate id="home.demo">Play with demo 🚀</Translate>
-          </Link>
-        </div>
-      </div>
-      <div className="banner__content">
-        <CodeSnippet
-          title={translate({
-            id: "home.annotate_design_widget",
-            message: "Annotate your widget",
-          })}
-          snippet={DesignSampleCode}
-        ></CodeSnippet>
-      </div>
-      <div className="banner__content">
-        <VideoSnippet
-          title={translate({
-            id: "home.debug_design_widget",
-            message: "Debug your widget",
-          })}
-          url='/vids/widget_viewer.gif'
-        ></VideoSnippet>
-      </div>
+      {codeSnippet()}
+      {widgetDesigner()}
     </header>
-  );
-};
+  </div>) : (<header className="banner">
+    <div className="banner__content">
+      {logo(siteConfig)}
+
+      {headline()}
+
+      <div>
+        {getStarted()}
+        {playWithDemo()}
+      </div>
+    </div>
+    {codeSnippet()}
+    {widgetDesigner()}
+  </header>);
+}
+
+function widgetDesigner() {
+  return <div className="banner__content">
+    <VideoSnippet
+      title={translate({
+        id: "home.debug_design_widget",
+        message: "Debug your widget"
+      })}
+      url='/vids/widget_viewer.gif'
+    ></VideoSnippet>
+  </div>;
+}
+
+function codeSnippet() {
+  return <div className="banner__content">
+    <CodeSnippet
+      title={translate({
+        id: "home.annotate_design_widget",
+        message: "Annotate your widget"
+      })}
+      snippet={DesignSampleCode}
+    ></CodeSnippet>
+  </div>;
+}
+
+function playWithDemo() {
+  return <Link className="banner__ctb" to="https://flutter-design-7b479.web.app/">
+    <Translate id="home.demo">Play with demo 🚀</Translate>
+  </Link>;
+}
+
+function getStarted() {
+  return <Link className="banner__cta" to={useBaseUrl("docs/get_started")}>
+    <Translate id="home.get_started">Get Started</Translate>
+  </Link>;
+}
+
+function headline() {
+  return <h1 className="banner__headline">
+    <Translate id="home.tagline">
+      Supercharge Flutter UX development
+    </Translate>
+  </h1>;
+}
+
+function logo(siteConfig) {
+  return <h1 className="banner__logo">
+    <img src="img/logo.png" alt="flutter_design" />
+    {siteConfig.title}
+  </h1>;
+}
+
+
+export const Banner = withBreakpoints(_Banner);
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,33 +7,46 @@ import { Feature } from "../components/Feature";
 import { Highlight } from "../components/Highlight";
 import { features } from "../data/features";
 import { highlights } from "../data/highlights";
+import ReactBreakpoints from 'react-breakpoints'
+
+const breakpoints = {
+  mobile: 320,
+  mobileLandscape: 480,
+  tablet: 768,
+  tabletLandscape: 1024,
+  desktop: 1200,
+  desktopLarge: 1500,
+  desktopWide: 1920,
+}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Supercharge your Flutter UX development">
-      <Banner />
-      <main>
-        <section>
-          <div className="highlight__container">
-            {highlights.map((props, index) => (
-              <Highlight key={`highlight-${index}`} {...props} />
-            ))}
-          </div>
-        </section>
+    <ReactBreakpoints breakpoints={breakpoints}>
+      <Layout
+        title={`${siteConfig.title}`}
+        description="Supercharge your Flutter UX development">
+        <Banner />
+        <main>
+          <section>
+            <div className="highlight__container">
+              {highlights.map((props, index) => (
+                <Highlight key={`highlight-${index}`} {...props} />
+              ))}
+            </div>
+          </section>
 
-        <section>
-          {features.map((props, index) => (
-            <Feature
-              key={`feature-${index}`}
-              {...props}
-              direction={index % 2 == 0 ? "normal" : "reverse"}
-            />
-          ))}
-        </section>
-      </main>
-    </Layout>
+          <section>
+            {features.map((props, index) => (
+              <Feature
+                key={`feature-${index}`}
+                {...props}
+                direction={index % 2 == 0 ? "normal" : "reverse"}
+              />
+            ))}
+          </section>
+        </main>
+      </Layout>
+    </ReactBreakpoints>
   );
 }
